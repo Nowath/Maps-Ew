00c2c90db537e324787950825533b3b3c53f2458
Nowath <nanoprocom@gmail.com>
2025-07-31 18:17:54 +0700

    fix: Comment out unused posts fetching in Server component

--- src/page/community/server.tsx
+++ src/page/community/server.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
-import { usePosts } from './api/posts'
+// import { usePosts } from './api/posts'
 import { Loading } from '@/components/loading/loading'
 
 const Client = dynamic(() => import('./client'))
 
 export default async function Server() {
-    const {posts} = await usePosts()
+    // const {posts} = await usePosts()
 
     return (
         <Suspense fallback={<Loading/>}>
